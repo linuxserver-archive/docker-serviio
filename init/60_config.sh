f4d5c93eb9d9d3ff891bd51a8e45bd0661c06296
sparklyballs <sparklyballs@gmail.com>
2015-11-02 22:37:21 +0000

    overnight save up

--- init/60_config.sh
+++ init/60_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p /config/serviio/config /config/serviio/plugins /config/serviio/library
+
 [[ -f /config/www/serviio/.htaccess ]] && rm /config/www/serviio/.htaccess
 [[ ! -L /app/serviio/plugins ]] && (cp -pr /app/serviio/plugins_orig/* /config/serviio/plugins/ && ln -s /config/serviio/plugins /app/serviio/plugins)
 [[ ! -f /config/serviio/config/application-profiles.xml || ! -f /config/serviio/config/log4j.xml || ! -f /config/serviio/config/profiles.xml ]] && cp /app/serviio/config/* /config/serviio/config/
 
-[[ ! -L /config/serviio/library/serviio.log ]] && ln -s /app/serviio/log/serviio.log /config/serviio/library/serviio.log
 chown -R abc:abc /app /config
