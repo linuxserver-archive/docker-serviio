37829c50665d29f4c71c207af7c19d57ef713f4b
sparklyballs <sparklyballs@gmail.com>
2015-11-02 20:47:46 +0000

    nearly there, got to solve the .htaccess file issue, link logs file and map media volumes

--- Dockerfile
+++ Dockerfile
@@ -29,7 +29,7 @@ apt-get clean && rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*
 #Adding Custom files
 ADD defaults/ /defaults/
 ADD init/ /etc/my_init.d/
-#ADD services/ /etc/service/
+ADD services/ /etc/service/
 RUN chmod -v +x /etc/service/*/run && chmod -v +x /etc/my_init.d/*.sh && \
 
 # give abc user a home folder
--- init/60_config.sh
+++ init/60_config.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+mkdir -p /config/serviio/config /config/serviio/plugins
 
-mkdir -p /config/serviio
+[[ ! -L /app/serviio/plugins ]] && (cp -pr /app/serviio/plugins_orig/* /config/serviio/plugins/ && ln -s /config/serviio/plugins /app/serviio/plugins)
+[[ ! -f /config/serviio/config/application-profiles.xml || ! -f /config/serviio/config/log4j.xml || ! -f /config/serviio/config/profiles.xml ]] && cp /app/serviio/config/* /config/serviio/config/
+
+
 chown -R abc:abc /app /config
