bb736362b01f7d258c8f56b4bb6931c4c52bd773
sparklyballs <sparklyballs@gmail.com>
2015-11-18 03:33:20 +0000

    sane config.sh init

--- init/60_config.sh
+++ init/60_config.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-mkdir -p /config/serviio/config /config/serviio/plugins /config/serviio/library
+mkdir -p /config/serviio/config /config/serviio/library
 
 [[ -f /config/www/serviio/.htaccess ]] && rm /config/www/serviio/.htaccess
-[[ ! -L /app/serviio/plugins ]] && (cp -pr /app/serviio/plugins_orig/* /config/serviio/plugins/ && ln -s /config/serviio/plugins /app/serviio/plugins)
+
+[[ ! -d /config/serviio/plugins ]] && cp -pr /app/serviio/plugins_orig /config/serviio/plugins
+
+[[ -L /app/serviio/plugins && ! /app/serviio/plugins -ef /config/serviio/plugins ]] && unlink /app/serviio/plugins
+
+[[ ! -L /app/serviio/plugins ]] && ln -s /config/serviio/plugins /app/serviio/plugins
+
 [[ ! -f /config/serviio/config/application-profiles.xml || ! -f /config/serviio/config/log4j.xml || ! -f /config/serviio/config/profiles.xml ]] && cp /app/serviio/config/* /config/serviio/config/
 
 chown -R abc:abc /app /config
