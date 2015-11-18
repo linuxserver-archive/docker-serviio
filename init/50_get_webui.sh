431c7f02499c5762ee4b98d2b9815698cd2c3652
sparklyballs <sparklyballs@gmail.com>
2015-11-18 03:39:23 +0000

    fix git pull init

--- init/50_get_webui.sh
+++ init/50_get_webui.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-[[ ! -d "/config/www/serviio/git" ]] && (git clone https://github.com/SwoopX/Web-UI-for-Serviio.git /config/www/serviio && cd /config/www/serviio && git checkout Serviio-1.5)
+[[ ! -d "/config/www/serviio/.git" ]] \
+&& (git clone https://github.com/SwoopX/Web-UI-for-Serviio.git /config/www/serviio \
+&& cd /config/www/serviio && git checkout Serviio-1.5)
 
 chown -R abc:abc /config/www/serviio
