79f25a638b5abcbf67344062589ce113b1f3b9e2
sparklyballs <sparklyballs@gmail.com>
2015-11-27 03:20:04 +0000

    set plugins folder in run statement instead of symlink, set transcode folder to /transcode, also in run statement

--- init/60_config.sh
+++ init/60_config.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+
 mkdir -p /config/serviio/config /config/serviio/library
 
 [[ -f /config/www/serviio/.htaccess ]] && rm /config/www/serviio/.htaccess
 
 [[ ! -d /config/serviio/plugins ]] && cp -pr /app/serviio/plugins_orig /config/serviio/plugins
 
-[[ -L /app/serviio/plugins && ! /app/serviio/plugins -ef /config/serviio/plugins ]] && unlink /app/serviio/plugins
-
-[[ ! -L /app/serviio/plugins ]] && ln -s /config/serviio/plugins /app/serviio/plugins
-
 [[ ! -f /config/serviio/config/application-profiles.xml || ! -f /config/serviio/config/log4j.xml || ! -f /config/serviio/config/profiles.xml ]] && cp /app/serviio/config/* /config/serviio/config/
 
 chown -R abc:abc /app /config
+chown abc:abc /transcode
+
